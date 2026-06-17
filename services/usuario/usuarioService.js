0f07394fa41bfe8f16c3e18499960fa5e0fd3a29
joseAllan <allan_santana2810@hotmail.com>
2026-06-17 09:27:29 -0300

    funcionalidade usuario

---
+++ services/usuario/usuarioService.js
@@ -0,0 +1,23 @@
+const mysqlConnection = require('../../models/dataBaseMysql');
+
+const buscarUsuarios = () => {
+    return new Promise((resolve, reject) => {
+
+        const sql = `
+            SELECT *
+            FROM tb_usuarios
+        `;
+
+        mysqlConnection.query(sql, (error, result) => {
+            if (error) {
+                return reject(error);
+            }
+
+            resolve(result);
+        });
+    });
+};
+
+module.exports = {
+    buscarUsuarios
+}
