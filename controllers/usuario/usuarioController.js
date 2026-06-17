8aacf1a52ffb17869c45ea80fad1164ec4b75373
joseAllan <allan_santana2810@hotmail.com>
2026-06-17 09:26:03 -0300

    funcionalidade usuario

---
+++ controllers/usuario/usuarioController.js
@@ -0,0 +1,18 @@
+const usuarioService = require('../../services/usuario/usuarioService');
+
+const listarUsuarios = async (req, res) => {
+    try {
+        const usuarios = await usuarioService.buscarUsuarios();
+        res.render('usuario/usuarios.ejs', {
+            usuarios: usuarios
+        });
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Erro ao carregar usuários');
+    }
+};
+
+module.exports = {
+    listarUsuarios
+}
